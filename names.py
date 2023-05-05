fc907f6345d34996da33dbae317fd6ad51397212
malabed187310 <mohammad.alabed@comed.com>
2023-05-05 16:06:06 -0500

    Create names.py

---
+++ names.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 15:59:16 2023
+
+@author: E187310
+"""
+print("Enter names of collaborators")
+print("----------------------------")
+print("")
+print("Mohammad")
